b08cd4bbc3b85b12773b9d77c271fb0e3bdf9291
Stefano Cerelli <hi@stefanocerelli.com>
2021-01-18 11:25:11 +0100

    Basic project settings, player movements

---
+++ Player/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+
+const ACCELLERATION = 500;
+const MAX_SPEED = 100;
+const FRICTION = 500;
+var velocity = Vector2.ZERO
+
+func _ready():
+	pass
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELLERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+	velocity = move_and_slide(velocity)
